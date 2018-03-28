a5dbc3fa628f159e60363351e8a1acf1ec9f5987
Parmarno <k.ekkarach@gmail.com>
2018-03-29 00:33:29 +0700

    add oauth

--- auth-access-rest-api.php
+++ auth-access-rest-api.php
@@ -13,7 +13,21 @@
 	 * License: GPL2+
 	 */
 
+	remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+	remove_action( 'template_redirect', 'rest_output_link_header', 11 );
 
 
+	add_filter( 'rest_authentication_errors', 'disable_json_rest', 20 );
 
+	function disable_json_rest(){
+		if (!isset($_SERVER['PHP_AUTH_USER']) && !isset($_SERVER['PHP_AUTH_PW'])) {
+			return new WP_Error( 'Permission Denied', __('You must send send user, password to access this api', 'auth-access-rest-api'), array( 'status' => 401 ) );
+		} else {
+			$tmp = wp_authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+			if ($tmp->errors) {
+				return new WP_Error( 'Permission Denied', __('You must send send user, password to access this api', 'auth-access-rest-api'), array( 'status' => 401 ) );
+			} 
+		}
+	}	
 
