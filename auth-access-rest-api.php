e1f72d194ae6d99819b40ff23daf3e49cef8a0bc
Parmarno <k.ekkarach@gmail.com>
2018-03-28 23:34:04 +0700

    add repo

---
+++ auth-access-rest-api.php
@@ -0,0 +1,19 @@
+<?php 
+	defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+	/**
+	 * Plugin Name: Auth Access Rest API
+	 * Plugin URI: https://github.com/Parmarno/auth-access-rest-api.git
+	 * Description: Closed WP Rest API Default and Create Auth to call api wp rest V2
+	 * Version: 1.0.0
+	 * Author: Ekkarach Kulwong
+	 * Author URI: http://www.i-makeweb.com
+	 * Text Domain: auth-access-rest-api
+	 * Domain Path: /languages
+	 * License: GPL2+
+	 */
+
+
+
+
+
